e6df72702b592627c21680363ecae5a7d3852885
Sarah <Sarah@DESKTOP-ARV4P1H.home>
2026-04-11 19:32:05 -0400

    Basic package built with these dependencies:
    spring web
    thymeleaf
    spring security
    spring data jpa
    h2 database
    Lombok
    rest repositories

---
+++ BookReviews/src/test/java/com/enterprise/java/BookReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
