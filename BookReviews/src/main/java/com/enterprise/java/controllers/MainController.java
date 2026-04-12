4172861915f14b2e8b2778b577a601164aab2904
Sarah <Sarah@DESKTOP-ARV4P1H.home>
2026-04-12 16:10:46 -0400

    Added repository, service, and security classes. Added a denial page

--- BookReviews/src/main/java/com/enterprise/java/controllers/MainController.java
+++ BookReviews/src/main/java/com/enterprise/java/controllers/MainController.java
@@ -1,8 +1,14 @@
 package com.enterprise.java.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
+	@RequestMapping("/denied")
+	public String denied(){
+	return "denied";
+	}
 
 }
