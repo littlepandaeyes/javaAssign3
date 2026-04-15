2341548d0e7089e239f814af489e5326cb7ab166
littlepandaeyes <grsaraha@sheridancollege.ca>
2026-04-15 17:23:39 -0400

    Insert book titles and reviews into data.sql
    Added multiple book entries and reviews to the database.

--- BookReviews/src/main/resources/data.sql
+++ BookReviews/src/main/resources/data.sql
@@ -1,3 +1,19 @@
 INSERT INTO MY_USER_DETAILS
 (USERNAME, PASSWORD, ROLES, ACCOUNT_NON_EXPIRED, ACCOUNT_NON_LOCKED, CREDENTIALS_NON_EXPIRED, ENABLED)
 VALUES('bob', 'pass', 'ROLE_USER', TRUE, TRUE, TRUE, TRUE);
+
+INSERT INTO book (title, author) VALUES ('Q-Squared', 'Peter David');
+INSERT INTO book (title, author) VALUES ('Silo', 'Hugh Howey');
+INSERT INTO book (title, author) VALUES ('Solo Leveling', 'Chu gong');
+
+INSERT INTO review (book_id, review) VALUES (1, 'fascinating');
+INSERT INTO review (book_id, review) VALUES (1, 'generational');
+INSERT INTO review (book_id, review) VALUES (1, 'could not put down');
+
+INSERT INTO review (book_id, review) VALUES (2, 'riveting');
+INSERT INTO review (book_id, review) VALUES (2, 'inspired');
+INSERT INTO review (book_id, review) VALUES (2, 'I would watch');
+
+INSERT INTO review (book_id, review) VALUES (3, 'fast paced');
+INSERT INTO review (book_id, review) VALUES (3, 'amazing art');
+INSERT INTO review (book_id, review) VALUES (3, 'need the next chapter');
